315321c0b06fdf07494ec02444edea496eeb693b
0ariell <arlmp135@gmail.com>
2025-04-01 16:31:21 -0300

    analityucs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
 import ProjectsPage from "./pages/ProjectsPage";
+// App.tsx o tu archivo principal de React
+
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
+      <Analytics />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
